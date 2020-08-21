1cd9f16257f0e2ca17026f5b7ffee7f707507203
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-08-21 20:13:49 +0000

    salida de error 0

--- built-in.c
+++ built-in.c
@@ -8,11 +8,11 @@
  */
 void built_in(char *string, char **argv, char **env, int *ex_it)
 {
-	if (_strcmp(argv[0], "exit") == 0)
+	if (_strcmp(argv[1], "exit") == 0)
 	{
 		free(argv);
 		free(string);
-		exit(2);
+		exit(*ex_it);
 	}
 	if (_strcmp(argv[0], "env") == 0)
 		printenv(env, ex_it);
